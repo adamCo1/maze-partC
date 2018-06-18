a656b1a798b38f7c8dd7a6bb073f007dc8448246
Adam cohen <adam388cohen@gmail.com>
2018-06-18 21:42:28 +0300

    bugs : 1.fix zoom 2.when a game is loaded , the focus is not set on the mazeDisplayer

--- src/View/Main.java
+++ src/View/Main.java
@@ -12,7 +12,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.input.ScrollEvent;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
